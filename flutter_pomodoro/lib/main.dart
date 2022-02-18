25d83d6b8e4165b6e0e0ac01c8fa5de0af9b0bd5
Larissa dos Santos Nunes - LNN <larissa.nunes@iteris.com.br>
2022-02-18 09:07:26 -0300

    Commit de teste

--- flutter_pomodoro/lib/main.dart
+++ flutter_pomodoro/lib/main.dart
@@ -14,15 +14,15 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(teste: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({Key? key, required this.teste}) : super(key: key);
 
-  final String title;
+  final String teste;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -41,7 +41,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(widget.teste),
       ),
       body: Center(
         child: Column(
